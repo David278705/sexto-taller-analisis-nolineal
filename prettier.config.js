50f92e94b342ecd3362557a5bf66256a3688ff0d
Jose David Lozano Valencia <josedavidlozanovalencia@MacBook-Air-de-Jose-4.local>
2025-01-25 10:33:19 -0500

    avance

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  trailingComma: "none",
+  tabWidth: 4,
+  singleQuote: true,
+  useTabs: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  proseWrap: "always",
+  endOfLine: "lf",
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  noBracketSpacing: true,
+  // printWidth: 100
+};
